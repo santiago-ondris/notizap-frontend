01f4663de07dedd838e039afd1351bc5e1ee26e5
santiago-ondris <santiagonicolas2001@gmail.com>
2025-06-03 13:34:40 -0300

    test to solve the issue

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
 // Interceptor para agregar el token automaticamente a cada request
 api.interceptors.request.use(
